fe87447125866ae7df4f12d9ca2de088fb862a98
Rory Flint <rory.flint@tipalti.com>
2024-02-06 06:22:25 +0000

    initial commit

---
+++ src/App.jsx
@@ -0,0 +1,10 @@
+function App() {
+  return (
+    <div>
+      <h1>React Starter Template</h1>
+      <p>For TypesScript, please checkout branch `typescript`</p>
+    </div>
+  );
+}
+
+export default App;
